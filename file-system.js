10d1cb34c1acbba5ac2b9ed95520a1ad35a3528f
itzzmeakhi <akhilmallidi.98@gmail.com>
2020-06-30 18:33:10 +0530

    Practised and pushed sample code on nodejs file system module

---
+++ file-system.js
@@ -0,0 +1,29 @@
+// To import Modules
+
+const fs = require('fs');
+
+// To read a file synchronously ( Blocking way )
+
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+
+console.log(textIn);
+
+// To write a file synchronously ( Blocking way )
+
+const textOut = `This is a new file. Data in it as follows: ${textIn}`;
+
+fs.writeFileSync('./txt/new-file.txt', textOut);
+
+// To read a file in asynchronous way ( Non Blocking way )
+
+
+fs.readFile('./txt/input.txt', 'utf-8', (err, data) => {
+    
+    console.log("Asynchronous data " + data);
+    fs.writeFile('./txt/output-async.txt', data, (err, data) => {
+        console.log("File was created successfully");
+    })
+
+});
+
+//console.log("Asynchronous Call ", textInAsync);
---
+++ hello-world.js
@@ -0,0 +1,3 @@
+const message = "Hello World from Node Js";
+
+console.log(message);
