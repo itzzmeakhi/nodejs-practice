8a5772c66e3f0db7ec79644f12fba6b3f1886b59
itzzmeakhi <akhilmallidi.98@gmail.com>
2020-07-01 10:31:44 +0530

    Added Routing to the sample-server file

--- sample-server.js
+++ sample-server.js
@@ -1,9 +1,24 @@
 // Import HTTP Module
 
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-    res.end("Hello from the server");
+    const pathName = req.url;
+    // console.log(req.url);
+
+    if(pathName === '/' || pathName === '/overview') {
+        res.end("This is an OVERVIEW Page");
+    } else if(pathName === '/product') {
+        res.end("This is an Product Page");
+    } else {
+        res.writeHead(404, {
+            'Content-type' : 'text/html',
+            'custom-header' : 'Resource not found'
+        });
+        res.end("<h1>Page not found</h1>");
+    }
+    // res.end("Hello from the server");
 })
 
 server.listen(8080, 'localhost', () => {
